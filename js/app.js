fbc7ad91b7580901a2e4a56d86b2adcb674f9aaf
unknown <a.heidet90400@gmail.com>
2019-10-14 22:04:41 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(function(){  // window.onload = (function(){
+var map = L.map('leaflet').setView([47.7475, 7.3375], 14);  /****coordonnée GPS Mulhouse + zoom*/
+var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { 
+            attribution: '© OpenStreetMap contributors',
+            maxZoom: 19
+        });
+    
+        map.addLayer(osmLayer);
+    })
