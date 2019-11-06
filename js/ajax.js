a03e1e4f93d5e03abf318d618c357ff6c135b598
Antoine <a.heidet90400@gmail.com>
2019-11-06 21:55:15 +0100

    POO request API jcdecaux

--- js/ajax.js
+++ js/ajax.js
@@ -17,3 +17,32 @@ function ajaxGet(url, callback) {
     req.send(null);
 }
 
+class Request {
+    constructor(callback, error){
+        this.req = new XMLHttpRequest();
+        this.req.addEventListener("load", function(data){
+            callback(data.target.responseText)
+        })
+        this.req.addEventListener("error", function(data){
+            error(data)
+        })
+    }
+    get(url){
+        this.req.open("GET",url)
+        this.req.send(null)
+    }
+}
+
+class ApiClient {
+    constructor(apiKey, contractName){
+        this.apiKey = apiKey;
+        this.contractName = contractName;
+    }
+    getStations(callback){
+        new Request(callback).get(`https://api.jcdecaux.com/vls/v1/stations?contract=${this.contractName}&apiKey=${this.apiKey}`)
+    }
+}
+const api = new ApiClient("b83d4fd83439b86791f32b1d4ee5e1c23a820009", "mulhouse")
+    api.getStations(function(data){
+        console.log(JSON.parse(data))
+    })
