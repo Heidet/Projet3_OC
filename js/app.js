0e89aec8d0be639967a92da6e28a38ae933931b0
Antoine <a.heidet90400@gmail.com>
2019-11-11 15:22:11 +0100

    ajout des marker sur la map

--- js/app.js
+++ js/app.js
@@ -1,36 +1,19 @@
-$(function(){  // window.onload = (function(){
-var map = L.map('leaflet').setView([47.7475, 7.3375], 14);  /****coordonnée GPS Mulhouse + zoom*/
-var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { 
-             attribution: '© OpenStreetMap contributors',
-            maxZoom: 19
-        });
-    
-        map.addLayer(osmLayer);
-    }) 
+let map = L.map('leaflet').setView([47.7475, 7.3375], 14);  /****coordonnée GPS Mulhouse + zoom*/
+let osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors',
+    maxZoom: 19
+});
+map.addLayer(osmLayer);
 
-    var map = L.map('cluster').setView([46.90296, 1.90925], 6);
 
-    var stamenToner = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
-        attribution: 'Map tiles by Stamen Design, CC BY 3.0 — Map data © OpenStreetMap',
-        subdomains: 'abcd',
-        minZoom: 0,
-        maxZoom: 20,
-        ext: 'png'
-    });
-    
-    map.addLayer(stamenToner);
-
-    var markersCluster = new L.MarkerClusterGroup();
-
+/**var markersCluster = new L.MarkerClusterGroup();
 var cities = getCities();
     for (var i = 0; i < cities.length; i++) {
         var latLng = new L.LatLng(cities[i][1], cities[i][2]);
         var marker = new L.Marker(latLng, {title: cities[i][0]});
         markersCluster.addLayer(marker);
     }
-
 map.addLayer(markersCluster);
-
 var markersClusterCustom = new L.MarkerClusterGroup({
     iconCreateFunction: function(cluster) {
         return L.divIcon({ 
@@ -40,7 +23,6 @@ var markersClusterCustom = new L.MarkerClusterGroup({
         });
     }
 });
-
 var markersClusterCustomPlus = new L.MarkerClusterGroup({
     iconCreateFunction: function(cluster) {
         var digits = (cluster.getChildCount()+'').length;
@@ -51,3 +33,4 @@ var markersClusterCustomPlus = new L.MarkerClusterGroup({
         });
     }
 });
+*/
