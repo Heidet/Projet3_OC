747f2a8f7a8c79c3c294dd48dafbc3db338201ef
Antoine <a.heidet90400@gmail.com>
2019-12-24 18:16:48 +0100

    changement fichier diapo pour mise en place POO sur diapo slider

--- js/diapo.js
+++ js/diapo.js
@@ -1,21 +1,22 @@
-window.onload = (function () {  // Exécute une fonction anonyme au chargement de la page (en gros le code dont ont à besoin)
-    const diapo = { // objet diapo 
-        playing: true,
-        slides: function () {
-            return document.getElementsByClassName('slide'); // retour les éléments class slide du DOM
-        },
-        length: function () {
+class Diapo {
+    constructor () {  // Exécute une fonction anonyme au chargement de la page (en gros le code dont ont à besoin)
+     // objet diapo 
+        this.playing =  true }
+        slides () {
+            return document.getElementsByClassName('slide') // retour les éléments class slide du DOM
+        }
+        length () {
             return this.slides().length; // retour nombre de slide '3'
-        },
-        index: function () {
+        }
+        index () {
             for (let i = 0; i < this.length(); i++) { // boucle for incrémention de 1 tant que longeur non atteinte
                 let slide = this.slides()[i]; // indice slide diapo 
                 if (slide.classList.contains('active')) { // si slide contient la class active 
                     return i;
                 }
             }
-        },
-        next: function () {
+        }
+        next () {
             let current = this.index(); // on récupère l'index actuel
             let next = current + 1; // on définis le nouveau (next) comme étant l'actuel + 1
             if (next > this.length() - 1) { // si next est supérieur à la taille de la liste -1 (en gros le numéro max)
@@ -23,8 +24,8 @@ window.onload = (function () {  // Exécute une fonction anonyme au chargement d
             }
             this.slides()[current].classList.remove('active'); // on supprime la classe active
             this.slides()[next].classList.add('active'); // on ajoute la classe active
-        },
-        previous: function () {
+        }
+        previous () {
             let current = this.index();  // appel de la méthode index dans mon objet diapo, attribution dans variable current 
             let previous = current - 1;
             if (previous < 0) { // si prévious inférieur à 0 = fin de diapo 
@@ -32,8 +33,8 @@ window.onload = (function () {  // Exécute une fonction anonyme au chargement d
             }
             this.slides()[current].classList.remove('active');
             this.slides()[previous].classList.add('active');
-        },
-        autoplay: function () { // méthode autoplay réutilisation méthode next. 
+        }
+        autoplay() { // méthode autoplay réutilisation méthode next. 
             let diapo = this;
             setInterval(function () {
                 if (diapo.playing) { // si playing vrai alors utilisation méthode next 
@@ -41,7 +42,7 @@ window.onload = (function () {  // Exécute une fonction anonyme au chargement d
                 }
             }, 5000); // setTime 5 secondes 
         }
-    };
+    }
 
     const key = document.addEventListener("keydown", function (e) {
         if (e.keyCode === 37) {
@@ -52,7 +53,7 @@ window.onload = (function () {  // Exécute une fonction anonyme au chargement d
         }
     });
 
-    diapo.autoplay(); // appel objet et méthode autoplay 
+     // appel objet et méthode autoplay 
 
     const left_button = new GenericButton(document.getElementById('cmd-left'), function () { // création variable bouton et attribution de son ID dans le DOM
         diapo.previous();
@@ -70,7 +71,8 @@ window.onload = (function () {  // Exécute une fonction anonyme au chargement d
             this.setClass("play");
         }
     });
-});
 
 
 
+const diapo = new Diapo(); 
+diapo.autoplay();
